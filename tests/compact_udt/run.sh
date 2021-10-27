99641e442cd0d8a7aad6e8d7bb7aa13444a0c985
Joii <joii@cryptape.com>
2021-10-27 12:26:23 +0800

    Add compact udt lock simulator tests

--- tests/compact_udt/run.sh
+++ tests/compact_udt/run.sh
@@ -1,2 +1,14 @@
+#!/bin/bash
 
+CUR_PWD=$(pwd)
+CUR_DIR=$(cd `dirname $0`; pwd)
+FOLDER=build-debug
 
+rm -rf $CUR_DIR/$FOLDER
+mkdir -p $CUR_DIR/$FOLDER
+cd $CUR_DIR/$FOLDER
+
+cmake -G "Unix Makefiles" ../
+make all
+./cudt_simulator
+cd $CUR_PWD
