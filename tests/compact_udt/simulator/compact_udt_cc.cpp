433cba9d8d96a765c6d77a14a5c1c215118281fe
Joii <joii@cryptape.com>
2021-11-06 20:20:54 +0800

    fix compact udt lock unit test: can not input simulator data

--- tests/compact_udt/simulator/compact_udt_cc.cpp
+++ tests/compact_udt/simulator/compact_udt_cc.cpp
@@ -31,9 +31,14 @@ uint8_t* cc_get_Data_by_field(CUDTMOL_Data* param) {
   if (param->field == CKB_CELL_FIELD_LOCK_HASH) {
     ptr = bin->scritp_hash_.get();
     param->len = bin->scritp_hash_.len();
-
-  } else if (param->field == CKB_CELL_FIELD_LOCK_HASH) {
-
+  } else if (param->field == CKB_CELL_FIELD_LOCK) {
+    const uint8_t data[] = {
+        0xFC, 0x9A, 0xBA, 0x69, 0x70, 0x10, 0xAD, 0x6D, 0x78, 0x19, 0xC6,
+        0xEB, 0x9B, 0x72, 0x96, 0xCB, 0xDA, 0x61, 0x5F, 0x10, 0x2C, 0x35,
+        0x1C, 0x8F, 0xC7, 0xAE, 0xF1, 0x2A, 0xEA, 0x6C, 0x9B, 0xEF,
+    };
+    ptr = (uint8_t*)data;
+    param->len = sizeof(data);
   } else {
     ASSERT_DBG(false);
     return NULL;
@@ -117,6 +122,5 @@ uint32_t cc_get_input_len() {
   ASSERT_DBG(vd);
   return vd->inputs_.size();
 }
-
 
 }  // extern "C"
