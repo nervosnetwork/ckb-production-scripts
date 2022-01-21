09ecb6ee93b72cc6280c1a804f4babadc09f3a6d
joii <joii@cryptape.com>
2022-01-21 10:43:52 +0800

    cardano-lock: Add successful unittest using rust

---
+++ tests/cardano_lock_rust/tests/misc.rs
@@ -0,0 +1,320 @@
+﻿use cardano_message_signing::utils::ToBytes;
+use cardano_message_signing::{builders::COSESign1Builder, HeaderMap, Headers, ProtectedHeaderMap};
+use cardano_serialization_lib::crypto::PrivateKey;
+use ckb_chain_spec::consensus::{Consensus, ConsensusBuilder};
+use ckb_script::TxVerifyEnv;
+use ckb_traits::{CellDataProvider, HeaderProvider};
+use ckb_types::{
+    bytes::{BufMut, Bytes, BytesMut},
+    core::{
+        cell::{CellMeta, CellMetaBuilder, ResolvedTransaction},
+        hardfork::HardForkSwitch,
+        Capacity, DepType, EpochNumberWithFraction, HeaderView, ScriptHashType, TransactionBuilder,
+        TransactionView,
+    },
+    packed::{
+        self, Byte32, CellDep, CellInput, CellOutput, OutPoint, Script, WitnessArgs,
+        WitnessArgsBuilder,
+    },
+    prelude::*,
+    H256,
+};
+use lazy_static::lazy_static;
+use rand::{thread_rng, Rng};
+use std::collections::HashMap;
+
+lazy_static! {
+    pub static ref CARDANO_LOCK_BIN: ckb_types::bytes::Bytes =
+        ckb_types::bytes::Bytes::from(include_bytes!("../../../build/cardano_lock").as_ref());
+    pub static ref ALWAYS_SUCCESS_BIN: ckb_types::bytes::Bytes =
+        ckb_types::bytes::Bytes::from(include_bytes!("../../../build/always_success").as_ref());
+}
+
+pub const SIGNATURE_SIZE: usize = 96;
+pub const MAX_CYCLES: u64 = std::u64::MAX;
+
+fn print_mem(d: &[u8]) {
+    let mut c = 0;
+    for i in 0..d.len() {
+        c = i;
+        print!("{:#04X}, ", d[i]);
+        if i % 16 == 15 {
+            print!("\n");
+        }
+    }
+    if c % 16 != 15 {
+        print!("\n");
+    }
+}
+
+pub fn dbg_print_mem(d: &[u8], n: &str) {
+    println!("{}, size:{}", n, d.len());
+    print_mem(d);
+}
+
+#[derive(Default)]
+pub struct DummyDataLoader {
+    pub cells: HashMap<OutPoint, (CellOutput, ckb_types::bytes::Bytes)>,
+}
+
+impl DummyDataLoader {
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
+
+impl CellDataProvider for DummyDataLoader {
+    // load Cell Data
+    fn load_cell_data(&self, cell: &CellMeta) -> Option<ckb_types::bytes::Bytes> {
+        cell.mem_cell_data.clone().or_else(|| {
+            self.cells
+                .get(&cell.out_point)
+                .map(|(_, data)| data.clone())
+        })
+    }
+
+    fn load_cell_data_hash(&self, cell: &CellMeta) -> Option<Byte32> {
+        self.load_cell_data(cell)
+            .map(|e| CellOutput::calc_data_hash(&e))
+    }
+
+    fn get_cell_data(&self, _out_point: &OutPoint) -> Option<ckb_types::bytes::Bytes> {
+        None
+    }
+
+    fn get_cell_data_hash(&self, _out_point: &OutPoint) -> Option<Byte32> {
+        None
+    }
+}
+
+impl HeaderProvider for DummyDataLoader {
+    fn get_header(&self, _hash: &Byte32) -> Option<HeaderView> {
+        None
+    }
+}
+
+pub fn blake160(message: &[u8]) -> Bytes {
+    let hash = Vec::from(&ckb_hash::blake2b_256(message)[..20]);
+    Bytes::from(hash)
+}
+
+pub fn sign_tx(tx: TransactionView, key: &PrivateKey) -> TransactionView {
+    let witnesses_len = tx.witnesses().len();
+    sign_tx_by_input_group(tx, key, 0, witnesses_len)
+}
+
+pub fn sign_tx_by_input_group(
+    tx: TransactionView,
+    key: &PrivateKey,
+    begin_index: usize,
+    len: usize,
+) -> TransactionView {
+    let tx_hash = tx.hash();
+    let mut signed_witnesses: Vec<packed::Bytes> = tx
+        .inputs()
+        .into_iter()
+        .enumerate()
+        .map(|(i, _)| {
+            if i == begin_index {
+                let mut blake2b = ckb_hash::new_blake2b();
+                let mut message = [0u8; 32];
+                blake2b.update(&tx_hash.raw_data());
+                // digest the first witness
+                let witness = WitnessArgs::new_unchecked(tx.witnesses().get(i).unwrap().unpack());
+                let zero_lock: Bytes = {
+                    let mut buf = Vec::new();
+                    buf.resize(SIGNATURE_SIZE, 0);
+                    buf.into()
+                };
+                let witness_for_digest = witness
+                    .clone()
+                    .as_builder()
+                    .lock(Some(zero_lock).pack())
+                    .build();
+                let witness_len = witness_for_digest.as_bytes().len() as u64;
+                blake2b.update(&witness_len.to_le_bytes());
+                blake2b.update(&witness_for_digest.as_bytes());
+                ((i + 1)..(i + len)).for_each(|n| {
+                    let witness = tx.witnesses().get(n).unwrap();
+                    let witness_len = witness.raw_data().len() as u64;
+                    blake2b.update(&witness_len.to_le_bytes());
+                    blake2b.update(&witness.raw_data());
+                });
+                blake2b.finalize(&mut message);
+                let message = H256::from(message);
+
+                let protected_serialized = ProtectedHeaderMap::new(&HeaderMap::new());
+                let headers = Headers::new(&protected_serialized, &HeaderMap::new());
+                let builder = COSESign1Builder::new(&headers, message.as_bytes().to_vec(), false);
+                let to_sign = builder.make_data_to_sign().to_bytes();
+
+                let sig = key.sign(&to_sign).to_bytes();
+                let mut witness_data: BytesMut = BytesMut::with_capacity(SIGNATURE_SIZE);
+
+                let pubkey = key.to_public();
+                witness_data.put(Bytes::from(pubkey.as_bytes()));
+                witness_data.put(Bytes::from(sig));
+
+                let witness_data = witness_data.freeze();
+                dbg_print_mem(&witness_data.to_vec(), "witness data");
+
+                witness
+                    .as_builder()
+                    .lock(Some(witness_data).pack())
+                    .build()
+                    .as_bytes()
+                    .pack()
+            } else {
+                tx.witnesses().get(i).unwrap_or_default()
+            }
+        })
+        .collect();
+    for i in signed_witnesses.len()..tx.witnesses().len() {
+        signed_witnesses.push(tx.witnesses().get(i).unwrap());
+    }
+    // calculate message
+    tx.as_advanced_builder()
+        .set_witnesses(signed_witnesses)
+        .build()
+}
+
+pub fn gen_tx(dummy: &mut DummyDataLoader, lock_args: Bytes) -> TransactionView {
+    let mut rng = thread_rng();
+    gen_tx_with_grouped_args(dummy, vec![(lock_args, 1)], &mut rng)
+}
+
+fn gen_tx_with_grouped_args<R: Rng>(
+    dummy: &mut DummyDataLoader,
+    grouped_args: Vec<(Bytes, usize)>,
+    rng: &mut R,
+) -> TransactionView {
+    let cardano_cell_data_hash = CellOutput::calc_data_hash(&CARDANO_LOCK_BIN);
+    // setup cardano dep
+    let cardano_data_out_point = {
+        let tx_hash = {
+            let buf = [0u8; 32];
+            //rng.fill(&mut buf);
+            buf.pack()
+        };
+        OutPoint::new(tx_hash, 0)
+    };
+    let cardano_data_cell = CellOutput::new_builder()
+        .capacity(
+            Capacity::bytes(CARDANO_LOCK_BIN.len())
+                .expect("data capacity")
+                .pack(),
+        )
+        .build();
+    dummy.cells.insert(
+        cardano_data_out_point.clone(),
+        (cardano_data_cell, CARDANO_LOCK_BIN.clone()),
+    );
+    // setup default tx builder
+    let dummy_capacity = Capacity::shannons(42);
+    let mut tx_builder = TransactionBuilder::default()
+        .cell_dep(
+            CellDep::new_builder()
+                .out_point(cardano_data_out_point)
+                .dep_type(DepType::Code.into())
+                .build(),
+        )
+        .output(
+            CellOutput::new_builder()
+                .capacity(dummy_capacity.pack())
+                .build(),
+        )
+        .output_data(Bytes::new().pack());
+
+    for (args, inputs_size) in grouped_args {
+        // setup dummy input unlock script
+        for _ in 0..inputs_size {
+            let previous_tx_hash = {
+                let buf = [1u8; 32];
+                //rng.fill(&mut buf);
+                buf.pack()
+            };
+            let previous_out_point = OutPoint::new(previous_tx_hash, 0);
+            let script = Script::new_builder()
+                .args(args.pack())
+                .code_hash(cardano_cell_data_hash.clone())
+                .hash_type(ScriptHashType::Data.into())
+                .build();
+            let previous_output_cell = CellOutput::new_builder()
+                .capacity(dummy_capacity.pack())
+                .lock(script)
+                .build();
+            dummy.cells.insert(
+                previous_out_point.clone(),
+                (previous_output_cell.clone(), Bytes::new()),
+            );
+            let mut random_extra_witness = [0u8; 32];
+            rng.fill(&mut random_extra_witness);
+            let witness_args = WitnessArgsBuilder::default()
+                .lock(Some(Bytes::from(random_extra_witness.to_vec())).pack())
+                .build();
+            tx_builder = tx_builder
+                .input(CellInput::new(previous_out_point, 0))
+                .witness(witness_args.as_bytes().pack());
+        }
+    }
+
+    tx_builder.build()
+}
+
+pub fn build_resolved_tx(
+    data_loader: &DummyDataLoader,
+    tx: &TransactionView,
+) -> ResolvedTransaction {
+    let resolved_cell_deps = tx
+        .cell_deps()
+        .into_iter()
+        .map(|deps_out_point| {
+            let (dep_output, dep_data) =
+                data_loader.cells.get(&deps_out_point.out_point()).unwrap();
+            CellMetaBuilder::from_cell_output(dep_output.to_owned(), dep_data.to_owned())
+                .out_point(deps_out_point.out_point())
+                .build()
+        })
+        .collect();
+
+    let mut resolved_inputs = Vec::new();
+    for i in 0..tx.inputs().len() {
+        let previous_out_point = tx.inputs().get(i).unwrap().previous_output();
+        let (input_output, input_data) = data_loader.cells.get(&previous_out_point).unwrap();
+        resolved_inputs.push(
+            CellMetaBuilder::from_cell_output(input_output.to_owned(), input_data.to_owned())
+                .out_point(previous_out_point)
+                .build(),
+        );
+    }
+
+    ResolvedTransaction {
+        transaction: tx.clone(),
+        resolved_cell_deps,
+        resolved_inputs,
+        resolved_dep_groups: vec![],
+    }
+}
+
+pub fn gen_tx_env() -> TxVerifyEnv {
+    let epoch = EpochNumberWithFraction::new(300, 0, 1);
+    let header = HeaderView::new_advanced_builder()
+        .epoch(epoch.pack())
+        .build();
+    TxVerifyEnv::new_commit(&header)
+}
+
+pub fn gen_consensus() -> Consensus {
+    let hardfork_switch = HardForkSwitch::new_without_any_enabled()
+        .as_builder()
+        .rfc_0032(200)
+        .build()
+        .unwrap();
+    ConsensusBuilder::default()
+        .hardfork_switch(hardfork_switch)
+        .build()
+}
+
+pub fn debug_printer(_script: &Byte32, msg: &str) {
+    print!("{}", msg);
+}
---
+++ tests/cardano_lock_rust/tests/test_cardano_lock.rs
@@ -0,0 +1,37 @@
+﻿use cardano_serialization_lib as csl;
+use ckb_script::TransactionScriptsVerifier;
+use ckb_types::bytes::{BufMut, BytesMut};
+use csl::crypto::PrivateKey;
+use misc::*;
+
+mod misc;
+
+#[test]
+fn test_success() {
+    let mut data_loader = DummyDataLoader::new();
+
+    let sk_bytes: [u8; 32] = [
+        34, 125, 55, 10, 222, 244, 31, 91, 181, 231, 62, 80, 90, 53, 246, 160, 226, 111, 123, 228,
+        188, 90, 15, 130, 210, 206, 78, 199, 209, 18, 202, 234,
+    ];
+
+    let privkey = PrivateKey::from_normal_bytes(&sk_bytes).unwrap();
+    let pubkey = privkey.to_public();
+    let pubkey_hash = blake160(&pubkey.as_bytes());
+    let mut identity: BytesMut = BytesMut::with_capacity(21);
+    identity.put_u8(0x7);
+    identity.put(pubkey_hash);
+
+    let tx = gen_tx(&mut data_loader, identity.freeze());
+    let tx = sign_tx(tx, &privkey);
+    let resolved_tx = build_resolved_tx(&data_loader, &tx);
+
+    let consensus = gen_consensus();
+    let tx_env = gen_tx_env();
+    let mut verifier =
+        TransactionScriptsVerifier::new(&resolved_tx, &consensus, &data_loader, &tx_env);
+    verifier.set_debug_printer(debug_printer);
+
+    let verify_result = verifier.verify(MAX_CYCLES);
+    verify_result.expect("pass verification");
+}
